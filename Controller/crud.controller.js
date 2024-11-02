b4631103eacf7831d833e99387d61c5746a3ad82
langeshkumar <langeshkumar8@gmail.com>
2024-11-02 09:20:36 +0530

    This is read

--- Controller/crud.controller.js
+++ Controller/crud.controller.js
@@ -1,15 +1,37 @@
 import crudmodel from "../Model/crud.model.js";
 
-export const crusRead = (req, res) => {
-    res.send({ msg: "This is read..!" });
+export const crusRead = async (req, res) => {
+    try {
+        const { name, age } = req.body;
+
+        if (!name && !age) {
+            res.status(401).json({ message: "Atleast one data required..!" });
+        }
+
+        const query = {};
+        if (name) query.name = name;
+        if (age) query.age = age;
+
+        const getCrudData = await crudmodel.find(query);
+
+        res.json(getCrudData);
+
+    } catch (error) {
+        res.status(400).json({
+            message: "Some think went wrong..!",
+            error: error.message
+        });
+    }
 };
 
 export const crudCreate = async (req, res) => {
     try {
+        const { name, age, description } = req.body;
+
         const crudInsert = new crudmodel({
-            name: req.body.name,
-            age: req.body.age,
-            description: req.body.description
+            name: name,
+            age: age,
+            description: description
         });
         const successmsg = await crudInsert.save();
 
